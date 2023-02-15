fc0941b097c91b37920f85cabf19bf48027d0ca9
pimplikarmaanas <mm.pimplikar@gmail.com>
2023-02-14 23:20:51 -0500

    implemented Adaptive A*

--- src/main.py
+++ src/main.py
@@ -5,10 +5,55 @@ import sys, time
 # main file
 sys.setrecursionlimit(12000)
 
+def get_random_pos():
+    dim = TEST_GRAPH_DIM
+    return random.randint(0, dim-1), random.randint(0, dim-1)
+
 if __name__ == "__main__":
     m = Maze()
-    # test_graphs = m.get_testing_graphs(count=50)
+    s = Search()
 
+    test_graphs = m.get_testing_graphs(count=20)
+    start_t = time.perf_counter()
+
+    avg_node_imp, avg_time_imp = 0, 0
+    paths_found = 0
+
+    set_test_dim(20)
+
+    for graph in test_graphs:
+        c_start, c_goal = get_random_pos(), get_random_pos()
+
+        print(f"############# GRAPH {graph.get_label()}")
+        repeated_path1, num_expanded1, duration1 = s.repeated_A_star(graph, c_start, c_goal)
+        if not repeated_path1:
+            print("Path not found using repeated A*...\n")
+            continue
+        # print(f"Found a path! Time taken: {duration1}")
+        # print(f"Path: {repeated_path1}")
+
+        repeated_path2, num_expanded2, duration2 = s.adaptive_A_star(graph, c_start, c_goal)
+        if not repeated_path2:
+            print("Path not found using adaptive A*...\n")
+            continue
+        # print(f"Found a path! Time taken: {duration2}")
+        # print(f"Path: {repeated_path2}")
+
+        time_imp, node_imp = (duration1 - duration2)/duration1, (num_expanded1 - num_expanded2)/num_expanded1
+        print(f"Adaptive A* expanded {node_imp: .2%} less nodes and improved time by {time_imp: .2%}\n")
+        paths_found += 1
+        avg_node_imp += node_imp
+        avg_time_imp += time_imp
+
+    end_t = time.perf_counter()
+    print(f"Time taken to complete all path test cases: {end_t - start_t}")
+    if paths_found:
+        avg_node_imp /= paths_found
+        avg_time_imp /= paths_found
+        print(f"On average, Adaptive A* expanded {avg_node_imp: .2%} less nodes and improved time by {time_imp: .2%}")
+
+
+    '''
     g = Graph(1, 5)
     g[(1, 2)].set_blocked(True)
     g[(2, 2)].set_blocked(True)
@@ -16,7 +61,4 @@ if __name__ == "__main__":
     g[(2, 3)].set_blocked(True)
     g[(3, 3)].set_blocked(True)
     g[(4, 3)].set_blocked(True)
-    
-    s = Search()
-    repeated_path = s.repeated_A_star(g, (4, 2), (4, 4))
-    print(repeated_path)
+    '''
--- src/search.py
+++ src/search.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
+
 from graph_util import *
 import heapq
+import time
 
 class Wrapper:
     def __init__(self, cord: tuple, g: int, f: int):
@@ -19,7 +22,7 @@ class Wrapper:
 
     def get_parent(self):
         return self.__parent
-    
+
     def set_parent(self, parent):
         self.__parent = parent
 
@@ -28,7 +31,7 @@ class Wrapper:
 
     def __lt__(self, other):
         return self.__f < other.get_f()
-    
+
     def __eq__(self, other):
         return self.__cord == other.__cord
 
@@ -53,25 +56,26 @@ class Search:
 
         return res
 
-    def __heuristic(self, start: tuple, end: tuple):
+    def __heuristic(self, added_costs: dict, start: tuple, end: tuple):
         # manhattan distance
         x1, y1 = start[0], start[1]
         x2, y2 = end[0], end[1]
 
-        return abs(x1-x2) + abs(y1-y2)
+        return abs(x1-x2) + abs(y1-y2) + added_costs[start] + added_costs[end]
 
-    def __compute_f(self, g: int, cur: tuple, goal: tuple):
-        return (g + 1), (g + 1) + self.__heuristic(cur, goal)
+    def __compute_f(self, added_costs: dict, g: int, cur: tuple, goal: tuple):
+        return (g + 1), (g + 1) + self.__heuristic(added_costs, cur, goal)
 
-    def A_star(self, maze: Graph, start: tuple, goal: tuple, known_blocks: set):
+    def A_star(self, maze: Graph, added_costs: dict, start: tuple, goal: tuple, known_blocks: set):
         # start and goal are tuples (i, j)
         open_list, closed_list = [Wrapper(start, 0, 0)], known_blocks.copy()
-        
+        expanded_nodes = 0
+
         while open_list:
+            expanded_nodes += 1
             cur_node = heapq.heappop(open_list)
 
             closed_list.add(cur_node.get_cord())
-            cur_g = cur_node.get_g()
 
             # terminating condition
             if cur_node.get_cord() == goal:
@@ -82,12 +86,12 @@ class Search:
                     cur_node = cur_node.get_parent()
 
                 # backtrack
-                return path[::-1]
-            
+                return path[::-1], expanded_nodes
+
             neighbors = self.__get_neighbors(maze, cur_node.get_cord())
-            
+
             for child in neighbors:
-                child_g, child_f = self.__compute_f(cur_node.get_g(), child, goal)
+                child_g, child_f = self.__compute_f(added_costs, cur_node.get_g(), child, goal)
 
                 child_wrapper = Wrapper(child, child_g, child_f)
                 child_wrapper.set_parent(cur_node)
@@ -103,25 +107,27 @@ class Search:
                         continue
 
                 heapq.heappush(open_list, child_wrapper)
-        
-        return None
+
+        return None, 0
 
     def repeated_A_star(self, maze: Graph, start: tuple, goal: tuple):
+        start_t = time.perf_counter()
+
         cur = start
         path_taken = []
         known_blocks = set()
-        
+        expanded_nodes = 0
+
         while cur != goal:
             # print("Known blocks: ", known_blocks)
-            path = self.A_star(maze, cur, goal, known_blocks)
+            path, cur_expanded = self.A_star(maze, defaultdict(int), cur, goal, known_blocks)
+            expanded_nodes += cur_expanded
             # print(f"currently on {cur}, projected path: {path}")
 
             if not path:
-                print("Can't get to end state.")
-                print("Path taken so far: ", path_taken)
-                return None
+                return None, None, None
 
-            i = 1            
+            i = 1
             while i < len(path) and not maze[path[i]].is_blocked():
                 known_blocks.add(cur)
                 cur = path[i]
@@ -132,4 +138,41 @@ class Search:
                 # print(f"Added {path[i]} to the known blocks")
                 known_blocks.add(path[i])
 
-        return path_taken
+        end_t = time.perf_counter()
+        return path_taken, expanded_nodes, end_t - start_t
+
+    def adaptive_A_star(self, maze: Graph, start: tuple, goal: tuple):
+        additional_costs = defaultdict(int)
+        start_t = time.perf_counter()
+
+        cur = start
+        path_taken = []
+        known_blocks = set()
+        expanded_nodes = 0
+
+        while cur != goal:
+            # print("Known blocks: ", known_blocks)
+            path, cur_expanded = self.A_star(maze, additional_costs, cur, goal, known_blocks)
+            if not path:
+                print("Path taken so far: ", path_taken)
+                return None, None, None
+
+            for node_t in path:
+                additional_costs[node_t] -= 1
+
+            expanded_nodes += cur_expanded
+            # print(f"currently on {cur}, projected path: {path}")
+
+            i = 1
+            while i < len(path) and not maze[path[i]].is_blocked():
+                known_blocks.add(cur)
+                cur = path[i]
+                path_taken.append(cur)
+                i += 1
+
+            if i < len(path) and maze[path[i]].is_blocked():
+                # print(f"Added {path[i]} to the known blocks")
+                known_blocks.add(path[i])
+
+        end_t = time.perf_counter()
+        return path_taken, expanded_nodes, end_t - start_t
